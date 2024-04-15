7b20d9e19cccf1d94790f81d9b512826e43f93fe
wangdeptrai <23020412@vnu.edu.vn>
2024-04-15 22:05:39 +0700

    Create dictionarySave.java

---
+++ src/main/java/Appfunction/save/dictionarySave.java
@@ -0,0 +1,29 @@
+package Appfunction.save;
+
+import Appfunction.Dictionaries.Word;
+import Appfunction.HTMLDictionaryCommanline;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class dictionarySave extends HTMLDictionaryCommanline {
+
+
+    public dictionarySave() throws IOException {
+    }
+    public void show(String word_target) {
+        ArrayList<Word> words = this.dictionarySearcher(word_target);
+        for (Word word : words) {
+            System.out.println(word.getWord_target());
+        }
+    }
+    public ArrayList<String> StingSearcher(String word_target) {
+        ArrayList<Word> words = this.dictionarySearcher(word_target);
+        ArrayList<String> result = new ArrayList<>();
+        for (Word word : words) {
+            result.add(word.getWord_target());
+        }
+        return result;
+    }
+}
