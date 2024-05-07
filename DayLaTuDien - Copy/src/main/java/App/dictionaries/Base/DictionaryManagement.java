ac0ed7154b2fe657721e30cb6b49a1dd71ca5c37
nkhanhtung <161182697+nkhanhtung@users.noreply.github.com>
2024-05-07 18:10:46 +0700

    Create DictionaryManagement.java

---
+++ DayLaTuDien - Copy/src/main/java/App/dictionaries/Base/DictionaryManagement.java
@@ -0,0 +1,56 @@
+package App.dictionaries.Base;
+
+import App.Supporting.ReadWordsFromFile;
+import App.Supporting.SortWords;
+import App.Supporting.WriteWordsToFile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class DictionaryManagement {
+    Dictionary dictionary = new Dictionary();
+    DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
+
+    public void insertFromCommandline() {
+        Scanner scanner = new Scanner(System.in);
+        String word_target = scanner.nextLine();
+        String word_explain = scanner.nextLine();
+        Word word = new Word(word_target, word_explain);
+        dictionary.add(word);
+    }
+
+    public void SaveWordToFile(ArrayList<Word> updatedWords) {
+        WriteWordsToFile pen = new WriteWordsToFile();
+        pen.write(updatedWords);
+    }
+
+    public void EditWord(String oldWord, String target, String explain) {
+        Word word_need_edit = dictionaryCommandline.lookup(oldWord);
+        ArrayList<Word> datta = new ArrayList<>();
+        datta = dictionaryCommandline.InsertFromFile();
+        for (Word word : datta) {
+            if (word.getWord_target().equals(oldWord)) {
+                word.setWord_target(target);
+                word.setWord_explain(explain);
+                break;
+            }
+        }
+        this.SaveWordToFile(datta);
+    }
+
+    public void DeleteWord(String delete) {
+        ArrayList<Word> tips = new ArrayList<>();
+        tips = dictionaryCommandline.InsertFromFile();
+        for (Word word : tips) {
+            if (word.getWord_target().equals(delete)) {
+                tips.remove(word);
+                break;
+            }
+        }
+        this.SaveWordToFile(tips);
+    }
+}
