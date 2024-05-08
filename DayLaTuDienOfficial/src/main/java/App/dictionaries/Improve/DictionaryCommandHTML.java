36a58cccedbb68692d9763a114fe114c3408c9f4
nkhanhtung <161182697+nkhanhtung@users.noreply.github.com>
2024-05-08 20:03:46 +0700

    Create DictionaryCommandHTML.java

---
+++ DayLaTuDienOfficial/src/main/java/App/dictionaries/Improve/DictionaryCommandHTML.java
@@ -0,0 +1,9 @@
+package App.dictionaries.Improve;
+
+import App.dictionaries.Base.DictionaryCommandline;
+
+public class DictionaryCommandHTML extends DictionaryCommandline {
+    public void DictionaryAdvanced() {
+
+    }
+}
