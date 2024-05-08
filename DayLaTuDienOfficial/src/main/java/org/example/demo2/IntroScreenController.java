cf23bbba01b2a6fb0233a87f16aff993cabfe5a1
nkhanhtung <161182697+nkhanhtung@users.noreply.github.com>
2024-05-08 20:01:20 +0700

    Create IntroScreenController.java

---
+++ DayLaTuDienOfficial/src/main/java/org/example/demo2/IntroScreenController.java
@@ -0,0 +1,40 @@
+package org.example.demo2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class IntroScreenController {
+    @FXML
+    private Button StartButton;
+    @FXML
+    private Button GameButton;
+
+    @FXML
+    public void onClicktolearn(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
+        Parent parent = fxmlLoader.load();
+        Scene learning_scene = new Scene(parent, 900, 650);
+        Stage primaryStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(learning_scene);
+        primaryStage.show();
+    }
+
+    @FXML
+    public void onClicktoGames(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("crossword.fxml"));
+        Parent game = fxmlLoader.load();
+        Scene game_scene = new Scene(game, 900, 650);
+        Stage primaryStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(game_scene);
+        primaryStage.show();
+    }
+}
