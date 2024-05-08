f3bc2e33f58cc3205632ecb36080519e35b86f54
nkhanhtung <161182697+nkhanhtung@users.noreply.github.com>
2024-05-08 20:01:10 +0700

    Create ReadWordsFromFile.java

---
+++ DayLaTuDienOfficial/src/main/java/App/Supporting/ReadWordsFromFile.java
@@ -0,0 +1,36 @@
+package App.Supporting;
+
+import App.dictionaries.Base.Word;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Dictionary;
+
+public class ReadWordsFromFile {
+
+    public static ArrayList<Word> read(String path) {
+        ArrayList<Word> Store = new ArrayList<>();
+        try {
+            File file = new File(path);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.trim().split("\\s+", 2);
+                if (parts.length >= 2) {
+                    String target = parts[0];
+                    String explain = parts[1];
+                    Store.add(new Word(target, explain));
+                }
+            }
+
+            bufferedReader.close();
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+        } catch (IOException e) {
+            System.out.println("Error when import from file");
+        }
+        return Store;
+    }
+}
